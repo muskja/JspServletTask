57dd3d6c0da29455b7ad74ca0fba6f3674817532
Muskan <jainmuskan9027@gmail.com>
2023-08-23 11:04:21 +0530

    final

--- src/main/java/com/example/servlet/AddressDeleteServlet.java
+++ src/main/java/com/example/servlet/AddressDeleteServlet.java
@@ -1,7 +1,6 @@
 package com.example.servlet;
 
 import com.example.util.DBUtil;
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 public class AddressDeleteServlet extends HttpServlet {
 
